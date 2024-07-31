2f404c812e593ad0f8a6aa010fc135e0742e2768
S672672 <bsmith672@rku.ac.in>
2024-07-31 23:27:42 +0530

    progress on the backend

---
+++ Backend/routes/auth.js
@@ -0,0 +1,13 @@
+// routes/auth.js
+
+const express = require('express');
+const { register, login } = require('../controllers/authController');
+const router = express.Router();
+
+// Register Route
+router.post('/register', register);
+
+// Login Route
+router.post('/login', login);
+
+module.exports = router;
