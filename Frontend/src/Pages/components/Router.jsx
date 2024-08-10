d25ac7f17c72eea39878503077ebdf77e40cfa31
S672672 <bsmith672@rku.ac.in>
2024-08-10 23:38:59 +0530

    authentication

--- Frontend/src/Pages/components/Router.jsx
+++ Frontend/src/Pages/components/Router.jsx
@@ -5,7 +5,6 @@ import Signup from "../Signup";
 import ConfirmSignin from "../ConfirmSignin";
 import InsideWeeklyad from "../InsideWeeklyad";
 import InWeekly from "../inweekly";
-import Grilling from "./Grilling";
 import GrillingPage from "../GrillingPage";
 
 const router = createBrowserRouter([
